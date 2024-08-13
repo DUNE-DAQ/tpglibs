03f0a67d4ea8d19aa50fd28c55f3af8340029ebf
Alejandro Oranday <alejandro@oran.day>
2024-08-13 08:43:57 +0200

    Add New Frame Expansion.
    Expansion is now in order and faster.

--- src/TPGenerator.cpp
+++ src/TPGenerator.cpp
@@ -28,48 +28,21 @@ __m256i
 TPGenerator::expand_frame(const __m256i& regi) {
   // Refer to the diagram and documentation on frame expansion for details.
 
-  // Rearrange original with row 3 doubled.
-  __m256i idx = _mm256_set_epi32(6, 5, 4, 3, 3, 2, 1, 0);
-  __m256i shuf1 = _mm256_permutevar8x32_epi32(regi, idx);
+  // Prepare even (2,4,6,8), odd (1,3,5,7) rows in 64-bit sense.
+  __m256i odd  = _mm256_permutevar8x32_epi32(regi, _mm256_setr_epi32(1, 0, 1, 2, 3, 4, 5, 6));
+  __m256i even = _mm256_permutevar8x32_epi32(regi, _mm256_setr_epi32(0, 1, 2, 3, 4, 5, 6, 7));
 
-  // Left shift each row.
-  __m256i count = _mm256_set_epi32(12, 8, 4, 0, 14, 10, 6, 2);
-  __m256i high_half = _mm256_sllv_epi32(shuf1, count);
-  high_half = _mm256_and_si256(high_half, _mm256_set1_epi32(0x3FFF0000u));  // Mask out the low half.
+  // Shift into place.
+  odd  = _mm256_srlv_epi64(odd, _mm256_setr_epi64x(30, 22, 14, 6));
+  even = _mm256_sllv_epi64(even, _mm256_setr_epi64x(6, 14, 22, 30));
 
-  // Left shift for low half later.
-  count = _mm256_set_epi32(10, 6, 2, 0, 12, 8, 4, 0);
-  __m256i shift2 = _mm256_sllv_epi32(shuf1, count);
+  // Everything is center aligned in 32-bit. Mask and right-align the right side.
+  __m256i both  = _mm256_blend_epi32(even, odd, 0b01010101);
+  __m256i right = _mm256_and_si256(_mm256_set1_epi32(0xFFFFu), both);
+  __m256i left  = _mm256_and_si256(_mm256_set1_epi32(0x3FFF0000u), both);
 
-  // Rearrange original and doubled rows 2 and 0.
-  idx = _mm256_set_epi32(5, 4, 3, 2, 2, 1, 0, 0);
-  __m256i shuf2 = _mm256_permutevar8x32_epi32(regi, idx);
-
-  // Right shift each row.
-  count = _mm256_set_epi32(22, 26, 30, 0, 20, 24, 28, 0);
-  __m256i shift3 = _mm256_srlv_epi32(shuf2, count);
-
-  // "Complete" the low half. Still more.
-  __m256i low_half = _mm256_or_si256(shift2, shift3);
-  low_half = _mm256_and_si256(low_half, _mm256_set1_epi32(0x3FFFu)); // Mask out the high half.
-
-  // Combine halves and clear space for an odd entry.
-  __m256i both = _mm256_or_si256(low_half, high_half);
-  both = _mm256_andnot_si256(_mm256_set_epi32(0, 0, 0, 0xFFFFu, 0, 0, 0, 0), both);
-
-  // There is a specific 16-bit entry that needs special handling.
-  // Align it.
-  __m256i shift4 = _mm256_srli_epi32(regi, 18);
-  // Mask it.
-  shift4 = _mm256_and_si256(_mm256_set_epi32(0, 0x3FFFu, 0, 0, 0, 0, 0, 0), shift4);
-
-  // Permute into the right spot
-  idx = _mm256_set_epi32(0, 0, 0, 6, 0, 0, 0, 0);
-  __m256i shuf3 = _mm256_permutevar8x32_epi32(shift4, idx);
-
-  // Add it in.
-  both = _mm256_or_si256(both, shuf3);
-  return both;
+  right = _mm256_srli_epi32(right, 2);
+  return _mm256_or_si256(left, right);
 }
 
 
