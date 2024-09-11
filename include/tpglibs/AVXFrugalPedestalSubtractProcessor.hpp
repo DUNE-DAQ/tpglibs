72395868e745ff56375cf3397f42c9fc97ab0197
Alejandro Oranday <alejandro@oran.day>
2024-09-11 19:01:09 +0200

    Change Initial Pedestal Value.
    Setting the initial pedestal to 14-bit max to avoid the initial garbage TPs.

--- include/tpglibs/AVXFrugalPedestalSubtractProcessor.hpp
+++ include/tpglibs/AVXFrugalPedestalSubtractProcessor.hpp
@@ -15,7 +15,7 @@ namespace tpglibs {
 
 class AVXFrugalPedestalSubtractProcessor : public AVXProcessor {
   protected:
-    __m256i m_pedestal = _mm256_setzero_si256();
+    __m256i m_pedestal = _mm256_set1_epi16(0x4000);  // Set initial start to 14-bit max. Prevents garbage TPs at start.
     __m256i m_accum = _mm256_setzero_si256();
     int16_t m_accum_limit{10};
 
