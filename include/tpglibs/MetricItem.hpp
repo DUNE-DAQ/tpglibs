69600d326e0f307bf15151164d7f5bbebd82c003
Theo Yu <xyz.yu@mail.utoronto.ca>
2025-07-02 16:47:39 +0200

    Add a struct to index our table in the buffer

--- include/tpglibs/MetricItem.hpp
+++ include/tpglibs/MetricItem.hpp
@@ -36,6 +36,18 @@ struct ChannelAwareSignalPointer {
 
 };
 
+struct MetricBufferKey {
+  
+  int16_t processor_id;
+  int16_t pipeline_id;
+  int16_t metric_id;
+  dunedaq::trgdataformats::channel_t channel_number;
+  
+  bool operator==(MetricBufferKey const& o) const noexcept {
+    return std::tie(processor_id, pipeline_id, metric_id, channel_number) == std::tie(o.processor_id, o.pipeline_id, o.metric_id, o.channel_number);
+  }
+};
+
 }//tpglibs
 
 namespace std {
@@ -45,6 +57,23 @@ namespace std {
       return (static_cast<size_t>(k.processor_id) << 32) ^
 	     (static_cast<size_t>(k.pipeline_id) << 16) ^
 	     (static_cast<size_t>(k.metric_id));
+    }
+  };
+
+  template <>
+  struct hash<tpglibs::MetricBufferKey> {
+    size_t operator()(tpglibs::MetricBufferKey const& k) const noexcept {
+      size_t h1 = std::hash<int16_t>{}(k.processor_id);
+      size_t h2 = std::hash<int16_t>{}(k.pipeline_id);
+      size_t h3 = std::hash<int16_t>{}(k.metric_id);
+      size_t h4 = std::hash<int16_t>{}(k.channel_number);
+
+      size_t seed = h1;
+      // pure magic
+      seed ^= h2 + 0x9e3779b97f4a7c15ULL + (seed << 6) + (seed >> 2);
+      seed ^= h3 + 0x9e3779b97f4a7c15ULL + (seed << 6) + (seed >> 2);
+      seed ^= h4 + 0x9e3779b97f4a7c15ULL + (seed << 6) + (seed >> 2);
+      return seed;
     }
   };
 }
