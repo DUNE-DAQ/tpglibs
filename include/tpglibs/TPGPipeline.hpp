0b26bfc9557db10813928be347d2c2a0c08dc8ba
Theo Yu <xyz.yu@mail.utoronto.ca>
2025-09-22 20:18:00 +0200

    Add get_all_processor_references method to TPGPipeline for retrieving all processor references in the pipeline

--- include/tpglibs/TPGPipeline.hpp
+++ include/tpglibs/TPGPipeline.hpp
@@ -11,6 +11,7 @@
 
 #include "tpglibs/AbstractFactory.hpp"
 #include "tpglibs/ProcessorMetricCollector.hpp"
+#include "tpglibs/AbstractProcessor.hpp"
 
 #include "trgdataformats/TriggerPrimitive.hpp"
 #include "trgdataformats/Types.hpp"
@@ -90,6 +91,23 @@ class TPGPipeline {
 
     /** @brief Pure virtual function that will generate TPs given a mask to draw from. */
     virtual std::vector<dunedaq::trgdataformats::TriggerPrimitive> generate_tps(const signal_t& tp_mask) = 0;
+
+    /**
+     * @brief Return reference to all processors in this pipeline.
+     *
+     * @return A vector of all processor references.
+     */
+    virtual std::vector<std::shared_ptr<AbstractProcessor<signal_t>>> get_all_processor_references() {
+      std::vector<std::shared_ptr<AbstractProcessor<signal_t>>> processor_references;
+      if (m_processor_head == nullptr) return processor_references;
+
+      auto current_processor = std::static_pointer_cast<AbstractProcessor<signal_t>>(m_processor_head);
+      while (current_processor != nullptr) {
+        processor_references.push_back(current_processor);
+        current_processor = current_processor->get_next_processor();
+      }
+      return processor_references;
+    }
 
     /** @brief Set the samples over threshold minimum values. */
     virtual void set_sot_minima(const std::vector<uint16_t>& sot_minima) {
